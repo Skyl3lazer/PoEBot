c6745829d825b26fcd45ae509c651c754ba3a44a
Derek Heinbach <derekjjjj@gmail.com>
2014-01-16 15:51:33 -0500

    Colon Update #BUGFIX - Twitch requires the ' :' procol be used for sending message

--- PoEBot.py
+++ PoEBot.py
@@ -62,7 +62,7 @@ def hourAlert():
    until = timeConverted - now
    NET = int(until.total_seconds())
    if until.total_seconds() > 0:
-    irc.send(bytes('PRIVMSG '+channel+' '+"EVENT ALERT - 1 HOUR - "+NextEvent['id'] +' - Occurs at '+NextEvent['startAt']+' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
+    irc.send(bytes('PRIVMSG '+channel+' :'+"EVENT ALERT - 1 HOUR - "+NextEvent['id'] +' - Occurs at '+NextEvent['startAt']+' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
     print("Starting Timer to event - "+str(NET-300))
     return [NET-300,'e']
    else:
@@ -77,7 +77,7 @@ def hourAlert():
     timeConverted=datetime.datetime(eventTimeY, eventTimeM, eventTimeD, eventTimeH, eventTimeMin, 0, 0).replace(tzinfo=ZULU)    
     until = timeConverted - now
     NET = int(until.total_seconds())
-    irc.send(bytes('PRIVMSG '+channel+' '+"EVENT ALERT - 1 HOUR - "+NextEvent['id'] +' - Occurs at '+NextEvent['startAt']+' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
+    irc.send(bytes('PRIVMSG '+channel+' :'+"EVENT ALERT - 1 HOUR - "+NextEvent['id'] +' - Occurs at '+NextEvent['startAt']+' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
     print("Starting Timer to next event - "+str(NET-300))
     return [NET-300,'e']
 def startAlert():
@@ -97,18 +97,18 @@ def startAlert():
    until = timeConverted - now
    NET = int(until.total_seconds())
    if until.total_seconds() > 3600:
-    irc.send(bytes('PRIVMSG '+channel+' '+"EVENT ALERT - STARTING IN 5 MINUTES - "+NextEvent['id'] +' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
+    irc.send(bytes('PRIVMSG '+channel+' :'+"EVENT ALERT - STARTING IN 5 MINUTES - "+NextEvent['id'] +' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
     print("Starting Timer to 1hr - " + str(NET-3600))
     return [NET-3600,'h']
    elif until.total_seconds() > 300:
-    irc.send(bytes('PRIVMSG '+channel+' '+"EVENT ALERT - STARTING IN 5 MINUTES - "+NextEvent['id'] +' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
+    irc.send(bytes('PRIVMSG '+channel+' :'+"EVENT ALERT - STARTING IN 5 MINUTES - "+NextEvent['id'] +' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
 	#TODO - Info about the race coming up in under an hour?
     print("Starting Timer to event - " + str(NET-300))
     return [NET-300,'e']
    else:
     print("Event Collision - SUB 5 MINUTE EVENT CHAIN")
     NextEvent=events[1];
-    irc.send(bytes('PRIVMSG '+channel+' '+"EVENT COLLISION ALERT - THERE MAY BE ERRORS HERE OR WITH THE NEXT ALERT - "+NextEvent['id'] +' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
+    irc.send(bytes('PRIVMSG '+channel+' :'+"EVENT COLLISION ALERT - THERE MAY BE ERRORS HERE OR WITH THE NEXT ALERT - "+NextEvent['id'] +' - '+NextEvent['url']+'\r\n', 'UTF-8')) #gives event info
     now = datetime.datetime.now(MYTZ)
     nextEventTime = events[2]['startAt']
     eventTimeY=int(nextEventTime[:4])
@@ -198,7 +198,7 @@ while 1:
         NET = until.seconds
         until=str(until)
         until = until[:-7]
-        irc.send(bytes('PRIVMSG '+channel+' EVENT IN PROGRESS - '+events[0]['id'] +' - Started at '+events[0]['startAt']+', ends at '+events[0]['endAt']+' - '+until+' remaining - '+events[0]['url']+'\r\n', 'UTF-8'))
+        irc.send(bytes('PRIVMSG '+channel+' :EVENT IN PROGRESS - '+events[0]['id'] +' - Started at '+events[0]['startAt']+', ends at '+events[0]['endAt']+' - '+until+' remaining - '+events[0]['url']+'\r\n', 'UTF-8'))
         nextEventTime = events[1]['startAt']
         eventTimeY=int(nextEventTime[:4])
         eventTimeM=int(nextEventTime[5:-13])
@@ -210,10 +210,10 @@ while 1:
         NET = until.seconds
         until=str(until)
         until = until[:-7]
-        irc.send(bytes('PRIVMSG '+channel+' '+events[1]['id'] +' - Occurs at '+events[1]['startAt']+', in \u0002'+until+'\u000F - '+events[1]['url']+'\r\n', 'UTF-8')) #gives event info
+        irc.send(bytes('PRIVMSG '+channel+' :'+events[1]['id'] +' - Occurs at '+events[1]['startAt']+', in \u0002'+until+'\u000F - '+events[1]['url']+'\r\n', 'UTF-8')) #gives event info
       else:
-        irc.send(bytes('PRIVMSG '+channel+' '+events[0]['id'] +' - Occurs at '+events[0]['startAt']+', in \u0002'+until+'\u000F - '+events[0]['url']+'\r\n', 'UTF-8')) #gives event info
+        irc.send(bytes('PRIVMSG '+channel+' :'+events[0]['id'] +' - Occurs at '+events[0]['startAt']+', in \u0002'+until+'\u000F - '+events[0]['url']+'\r\n', 'UTF-8')) #gives event info
    if text.find(b':!help') != -1:   #Check for '!help' command
      print("Command found - Help")
-     irc.send(bytes('PRIVMSG '+channel+' '+"!next - Displays next upcoming event, and any currently running event"+'\r\n', 'UTF-8'))
+     irc.send(bytes('PRIVMSG '+channel+' :'+"!next - Displays next upcoming event, and any currently running event"+'\r\n', 'UTF-8'))
    #Add any other commands you wish!
